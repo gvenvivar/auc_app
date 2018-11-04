2e98dd520c91bb65cc3116eab6f53a6a9344b6a1
gven <gven.inc@gmail.com>
2018-11-04 13:57:16 +0200

    update buld with react 16.6 and other nrw packages

---
+++ my-app/build/precache-manifest.624157e6cd133243ce1d5961923f8f12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d5c2bec0b7f20b3682c872a7ec3d492",
+    "url": "/static/media/loading2.7d5c2bec.gif"
+  },
+  {
+    "revision": "830ac1e32ba1277d52827756175377ee",
+    "url": "/static/media/frostmourne_by_chillalord-d5ka9vy_dark.830ac1e3.jpg"
+  },
+  {
+    "revision": "ae7ab87e49e4be9a320e652134749ee7",
+    "url": "/static/media/close.ae7ab87e.png"
+  },
+  {
+    "revision": "ed73789884f7826d794c054d0918bea2",
+    "url": "/static/media/cerrar2.ed737898.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ee71b366038b78a2e5f",
+    "url": "/static/js/main.8ee71b36.chunk.js"
+  },
+  {
+    "revision": "ec391210be25412ab94e",
+    "url": "/static/js/1.ec391210.chunk.js"
+  },
+  {
+    "revision": "8ee71b366038b78a2e5f",
+    "url": "/static/css/main.4e0cfaba.chunk.css"
+  },
+  {
+    "revision": "ce36e9146b3f415b541c7c59bc71e333",
+    "url": "/index.html"
+  }
+];
