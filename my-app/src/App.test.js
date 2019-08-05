be28fc7a841fff7bc007d5554e1aa48723671f19
gven <gven.inc@gmail.com>
2019-08-05 15:04:31 +0300

    add large logo img

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import functions from './functions';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('cut email', ()=>{
+  expect(functions.cutEmail('umka@gmail.com').toBe('umka'));
+})
